aead24d0ebd43533867f0242492d65e6c6d674ea
Vishal <vishalrohera07@gmail.com>
2025-01-22 14:27:28 +0530

    Q6 Pushed0

---
+++ Q6.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+    let newarr = [];
+    for (const element of arr) {
+        newarr.push(fn(element+1)); // Apply fn to each element
+    }
+    console.log("Output array:", newarr); // Log the final output
+    return newarr;
+};
+
+// Define fn
+const fn = function (value) {
+    console.log("Processing value:", value); // Log the current value being processed
+    return value
+};
+
+// Call map with fn
+map([1, 2, 3], fn);
