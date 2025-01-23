fbd6d6f8bc8b1e6199025fd8e0b671f929608c99
Vishal <vishalrohera07@gmail.com>
2025-01-23 18:55:52 +0530

    Q10 pushed

---
+++ Q10.js
@@ -0,0 +1,16 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    let count=args
+    
+    return console.log(count.length);
+    
+    
+};
+
+argumentsLength(1, 2, 3);
+
+  ; // 3
+ 
