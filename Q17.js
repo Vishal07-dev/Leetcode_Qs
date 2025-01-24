0a7f8c5070adf261d5a69e1350100e41f3f5364c
Vishal <vishalrohera07@gmail.com>
2025-01-24 14:22:52 +0530

    Q17 pushed

---
+++ Q17.js
@@ -0,0 +1,35 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    
+    return async function(...args) {
+     return new Promise((res,rej)=>{
+
+            Promise.resolve(fn(...args))
+            .then((result)=>{
+                clearTimeout(timer)
+                res(result)
+            })
+            // .catch(()=>{
+
+            // })
+             .catch((err) => {
+                    clearTimeout(timer); 
+                    rej(err); 
+                });
+
+            let timer = setTimeout(()=>
+            {
+                rej("Time Limit Exceeded")
+            },t)
+     })   
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */s
