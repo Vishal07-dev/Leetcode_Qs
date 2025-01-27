554e54612dea9ea26e71712081ba59c9fe8b4d93
Vishal <vishalrohera07@gmail.com>
2025-01-27 18:35:07 +0530

    Q18  pushed

---
+++ Q18.js
@@ -0,0 +1,61 @@
+var TimeLimitedCache = function() {
+    this.map = new Map();
+};
+
+/** 
+* @param {number} key
+* @param {number} value
+* @param {number} duration time until expiration in ms
+* @return {boolean} if un-expired key already existed
+*/
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+
+let haveval =  this.map.has(key);
+if(haveval) clearTimeout(this.map.get(key).timer);
+
+
+
+let timer= setTimeout(()=>{
+    this.map.delete(key)
+},duration)
+
+
+   this.map.set(key, { value, timer });
+
+
+return haveval
+
+
+};
+
+/** 
+* @param {number} key
+* @return {number} value associated with key
+*/
+TimeLimitedCache.prototype.get = function(key) {
+
+const stored = this.map.get(key)
+
+if(stored)
+{
+return stored.value
+}
+else
+{
+return -1
+}
+};
+
+/** 
+* @return {number} count of non-expired keys
+*/
+TimeLimitedCache.prototype.count = function() {
+return this.map.size
+};
+
+/**
+* const timeLimitedCache = new TimeLimitedCache()
+* timeLimitedCache.set(1, 42, 1000); // false
+* timeLimitedCache.get(1) // 42
+* timeLimitedCache.count() // 1
+*/
