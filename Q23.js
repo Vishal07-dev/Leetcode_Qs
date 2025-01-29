49d865c71ffd2d0ee0a1255d874c7eaaf0dc85f6
Vishal <vishalrohera07@gmail.com>
2025-01-29 18:23:37 +0530

    2 Q pushed

---
+++ Q23.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    let obj = {} 
+     this.map((item)=>{
+       let key = fn(item)
+       obj[key]=obj[key]||[]
+       obj[key] ? obj[key].push(item) : obj[key] = item
+    })
+    return obj
+    };
+    
+    /**
+     * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+     */
